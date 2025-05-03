e567678e1db21a14afff21320f459d371afbd825
Renato Oliveira <renato.oliveira@dogeforce.com>
2025-05-02 23:54:56 -0300

    feat: initial work on getting permission sets not assigned to the user listed as quick pick items and assigned upon selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,19 @@
 import * as vscode from 'vscode';
 import { activate as activateLabelsAutoComplete } from './commands/labels/load';
 import { activateLabelCreateOnPalette } from './commands/labels/create';
+import { activate as activatePermissionSetCommands } from './commands/permsets/load';
 
 export function activate(context: vscode.ExtensionContext) {
+    // #region labels
+
     activateLabelsAutoComplete(context);
     activateLabelCreateOnPalette(context);
+
+    // #endregion
+
+    // #region permission sets
+
+    activatePermissionSetCommands(context);
+
+    // #endregion
 }
