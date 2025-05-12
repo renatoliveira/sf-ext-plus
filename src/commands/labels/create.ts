3f56ebd2f2db6c860a6b0d0f118bcb88f07fd01b
Renato Oliveira <renato.oliveira@dogeforce.com>
2025-05-12 10:13:00 -0300

    fix: fixes TS issue with the create command

--- src/commands/labels/create.ts
+++ src/commands/labels/create.ts
@@ -198,147 +198,156 @@ export async function activateLabelCreateOnPalette(context: vscode.ExtensionCont
 }
 
 export async function getLabelCreateOnCodeActionProvider() {
+    const provider: vscode.CodeActionProvider<vscode.CodeAction> = {
+        provideCodeActions(
+            document: vscode.TextDocument,
+            range: vscode.Range | vscode.Selection,
+            context: vscode.CodeActionContext,
+            token: vscode.CancellationToken
+        ): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
+            const result: vscode.CodeAction[] = [];
+
+            let proposedLabelValue = document.getText(range);
+
+            // if label is empty, return
+            if (!proposedLabelValue) {
+                return;
+            }
+
+            // if label is surrounded by quotes, remove them
+            if (proposedLabelValue.startsWith('\'') && proposedLabelValue.endsWith('\'')) {
+                proposedLabelValue = proposedLabelValue.slice(1, -1);
+            }
+
+            // Check if the label name exists in the list of labels
+            if (getSalesforceLabelsStore().salesforceLabels[proposedLabelValue]) {
+                // if it does, do nothing
+                return;
+            }
+
+            // check if the content exists as the value of a label already
+            const existingLabel = Object.values(getSalesforceLabelsStore().salesforceLabels).find(label => label.value[0] === proposedLabelValue);
+
+            // check if its text shows on some other label in part too
+            const similarLabels = Object.values(getSalesforceLabelsStore().salesforceLabels).filter(label =>
+                label.value[0].includes(proposedLabelValue) && (!!existingLabel ? label.value[0] !== existingLabel.value[0] : true)
+            );
+
+            if (existingLabel) {
+                // if it does, suggest using the existing label
+                const useExistingLabelAction = new vscode.CodeAction(
+                    labels.commands.USE_EXISTING_LABEL(existingLabel.fullName.toString()),
+                    vscode.CodeActionKind.QuickFix
+                );
+                useExistingLabelAction.isPreferred = true;
+                useExistingLabelAction.title = labels.commands.USE_EXISTING_LABEL(existingLabel.fullName.toString());
+                useExistingLabelAction.kind = vscode.CodeActionKind.QuickFix;
+                useExistingLabelAction.edit = new vscode.WorkspaceEdit();
+
+                // if the existing range starts and ends with a single quote, adjust the range
+                // so they are replaced too
+                if (existingLabel.value[0].startsWith('\'') && existingLabel.value[0].endsWith('\'')) {
+                    const startLine = document.lineAt(range.start.line).text.indexOf('\'');
+                    const endLine = document.lineAt(range.start.line).text.indexOf('\'', startLine + 1) + 1;
+                    const rangeToReplace = new vscode.Range(
+                        range.start.line,
+                        startLine,
+                        range.start.line,
+                        endLine
+                    );
+
+                    useExistingLabelAction.edit.replace(document.uri, rangeToReplace, `System.Label.${existingLabel.fullName.toString()}`);
+                } else {
+                    // just replace the range
+                    useExistingLabelAction.edit.replace(document.uri, range, `System.Label.${existingLabel.fullName.toString()}`);
+                }
+
+                const alreadyExistsMessage = labels.commands.LABEL_ALREADY_EXISTS_AS(proposedLabelValue.toString(), existingLabel.fullName.toString());
+
+                // suggest the quick fix to use the existing label
+                useExistingLabelAction.diagnostics = [
+                    new vscode.Diagnostic(range, alreadyExistsMessage, vscode.DiagnosticSeverity.Information)
+                ];
+                useExistingLabelAction.diagnostics[0].source = labels.misc.EXTENSION_SOURCE_NAME;
+                useExistingLabelAction.diagnostics[0].code = `${labels.misc.EXTENSION_NAME}.useExistingLabel`;
+                useExistingLabelAction.diagnostics[0].relatedInformation = [
+                    new vscode.DiagnosticRelatedInformation(
+                        new vscode.Location(document.uri, range),
+                        alreadyExistsMessage
+                    )
+                ];
+
+                // Add the action to the list of code actions
+                result.push(useExistingLabelAction);
+            }
+
+            // for each similar label found, suggest using it
+            if (similarLabels && similarLabels.length > 0) {
+                const similarLabelActions = similarLabels.map(label => {
+                    const useSimilarLabelMessage = labels.commands.USE_SIMILAR_LABEL(label.fullName.toString());
+                    const useSimilarLabelAction = new vscode.CodeAction(
+                        useSimilarLabelMessage,
+                        vscode.CodeActionKind.QuickFix
+                    );
+                    useSimilarLabelAction.isPreferred = true;
+                    useSimilarLabelAction.title = useSimilarLabelMessage;
+                    useSimilarLabelAction.kind = vscode.CodeActionKind.QuickFix;
+                    useSimilarLabelAction.edit = new vscode.WorkspaceEdit();
+
+                    // the range to replace needs to be calculated based on the line, to replace
+                    // the whole string between simple quotes in Apex
+                    const startLine = document.lineAt(range.start.line).text.indexOf('\'');
+                    const endLine = document.lineAt(range.start.line).text.indexOf('\'', startLine + 1) + 1;
+                    const rangeToReplace = new vscode.Range(
+                        range.start.line,
+                        startLine,
+                        range.start.line,
+                        endLine
+                    );
+
+                    // replaces the selected text with the label
+                    useSimilarLabelAction.edit.replace(document.uri, rangeToReplace, `System.Label.${label.fullName.toString()}`);
+
+                    // Add the action to the list of code actions
+                    return useSimilarLabelAction;
+                });
+
+                // Add the action to the list of code actions
+                result.push(...similarLabelActions);
+            }
+
+            // else, suggest creating a new label from the selected text
+            const createLabelForProposedValueMessage = labels.commands.CREATE_LABEL_FOR_PROPOSED_VALUE(proposedLabelValue.toString());
+
+            const createLabelAction = new vscode.CodeAction(
+                createLabelForProposedValueMessage,
+                vscode.CodeActionKind.QuickFix
+            );
+            createLabelAction.command = {
+                command: `${labels.misc.EXTENSION_NAME}.createNewLabel`,
+                title: labels.commands.CREATE_LABEL_CODE_ACTION_TITLE,
+                arguments: [proposedLabelValue]
+            };
+            createLabelAction.isPreferred = true;
+            createLabelAction.title = createLabelForProposedValueMessage;
+            createLabelAction.kind = vscode.CodeActionKind.QuickFix;
+
+            // Add the action to the list of code actions
+            result.push(createLabelAction);
+
+            // return all available actions
+            return result;
+        }
+    } as vscode.CodeActionProvider;
+
+    // Register the CodeActionProvider for specific file types
     const labelContextMenuProvider = vscode.languages.registerCodeActionsProvider(
         [
             { scheme: 'file', pattern: '**/*.cls' },
             { scheme: 'file', language: 'apex' }
         ],
-        {
-            provideCodeActions(document, range, _context) {
-                const result: vscode.CodeAction[] = [];
-
-                let proposedLabelValue = document.getText(range);
-
-                // if label is empty, return
-                if (!proposedLabelValue) {
-                    return;
-                }
-
-                // if label is surrounded by quotes, remove them
-                if (proposedLabelValue.startsWith('\'') && proposedLabelValue.endsWith('\'')) {
-                    proposedLabelValue = proposedLabelValue.slice(1, -1);
-                }
-
-                // Check if the label name exists in the list of labels
-                if (getSalesforceLabelsStore().salesforceLabels[proposedLabelValue]) {
-                    // if it does, do nothing
-                    return;
-                }
-
-                // check if the content exists as the value of a label already
-                const existingLabel = Object.values(getSalesforceLabelsStore().salesforceLabels).find(label => label.value[0] === proposedLabelValue);
-
-                // check if its text shows on some other label in part too
-                const similarLabels = Object.values(getSalesforceLabelsStore().salesforceLabels).filter(label =>
-                    label.value[0].includes(proposedLabelValue) && (!!existingLabel ? label.value[0] !== existingLabel.value[0] : true)
-                );
-
-                if (existingLabel) {
-                    // if it does, suggest using the existing label
-                    const useExistingLabelAction = new vscode.CodeAction(
-                        labels.commands.USE_EXISTING_LABEL(existingLabel.fullName.toString()),
-                        vscode.CodeActionKind.QuickFix
-                    );
-                    useExistingLabelAction.isPreferred = true;
-                    useExistingLabelAction.title = labels.commands.USE_EXISTING_LABEL(existingLabel.fullName.toString());
-                    useExistingLabelAction.kind = vscode.CodeActionKind.QuickFix;
-                    useExistingLabelAction.edit = new vscode.WorkspaceEdit();
-
-                    // if the existing range starts and ends with a single quote, adjust the range
-                    // so they are replaced too
-                    if (existingLabel.value[0].startsWith('\'') && existingLabel.value[0].endsWith('\'')) {
-                        const startLine = document.lineAt(range.start.line).text.indexOf('\'');
-                        const endLine = document.lineAt(range.start.line).text.indexOf('\'', startLine + 1) + 1;
-                        const rangeToReplace = new vscode.Range(
-                            range.start.line,
-                            startLine,
-                            range.start.line,
-                            endLine
-                        );
-
-                        useExistingLabelAction.edit.replace(document.uri, rangeToReplace, `System.Label.${existingLabel.fullName.toString()}`);
-                    } else {
-                        // just replace the range
-                        useExistingLabelAction.edit.replace(document.uri, range, `System.Label.${existingLabel.fullName.toString()}`);
-                    }
-
-                    const alreadyExistsMessage = labels.commands.LABEL_ALREADY_EXISTS_AS(proposedLabelValue.toString(), existingLabel.fullName.toString());
-
-                    // suggest the quick fix to use the existing label
-                    useExistingLabelAction.diagnostics = [
-                        new vscode.Diagnostic(range, alreadyExistsMessage, vscode.DiagnosticSeverity.Information)
-                    ];
-                    useExistingLabelAction.diagnostics[0].source = labels.misc.EXTENSION_SOURCE_NAME;
-                    useExistingLabelAction.diagnostics[0].code = `${labels.misc.EXTENSION_NAME}.useExistingLabel`;
-                    useExistingLabelAction.diagnostics[0].relatedInformation = [
-                        new vscode.DiagnosticRelatedInformation(
-                            new vscode.Location(document.uri, range),
-                            alreadyExistsMessage
-                        )
-                    ];
-
-                    // Add the action to the list of code actions
-                    result.push(useExistingLabelAction);
-                }
-
-                // for each similar label found, suggest using it
-                if (similarLabels && similarLabels.length > 0) {
-                    const similarLabelActions = similarLabels.map(label => {
-                        const useSimilarLabelMessage = labels.commands.USE_SIMILAR_LABEL(label.fullName.toString());
-                        const useSimilarLabelAction = new vscode.CodeAction(
-                            useSimilarLabelMessage,
-                            vscode.CodeActionKind.QuickFix
-                        );
-                        useSimilarLabelAction.isPreferred = true;
-                        useSimilarLabelAction.title = useSimilarLabelMessage;
-                        useSimilarLabelAction.kind = vscode.CodeActionKind.QuickFix;
-                        useSimilarLabelAction.edit = new vscode.WorkspaceEdit();
-
-                        // the range to replace needs to be calculated based on the line, to replace
-                        // the whole string between simple quotes in Apex
-                        const startLine = document.lineAt(range.start.line).text.indexOf('\'');
-                        const endLine = document.lineAt(range.start.line).text.indexOf('\'', startLine + 1) + 1;
-                        const rangeToReplace = new vscode.Range(
-                            range.start.line,
-                            startLine,
-                            range.start.line,
-                            endLine
-                        );
-
-                        // replaces the selected text with the label
-                        useSimilarLabelAction.edit.replace(document.uri, rangeToReplace, `System.Label.${label.fullName.toString()}`);
-
-                        // Add the action to the list of code actions
-                        return useSimilarLabelAction;
-                    });
-
-                    // Add the action to the list of code actions
-                    result.push(...similarLabelActions);
-                }
-
-                // else, suggest creating a new label from the selected text
-                const createLabelForProposedValueMessage = labels.commands.CREATE_LABEL_FOR_PROPOSED_VALUE(proposedLabelValue.toString());
-
-                const createLabelAction = new vscode.CodeAction(
-                    createLabelForProposedValueMessage,
-                    vscode.CodeActionKind.QuickFix
-                );
-                createLabelAction.command = {
-                    command: `${labels.misc.EXTENSION_NAME}.createNewLabel`,
-                    title: labels.commands.CREATE_LABEL_CODE_ACTION_TITLE,
-                    arguments: [proposedLabelValue]
-                };
-                createLabelAction.isPreferred = true;
-                createLabelAction.title = createLabelForProposedValueMessage;
-                createLabelAction.kind = vscode.CodeActionKind.QuickFix;
-
-                // Add the action to the list of code actions
-                result.push(createLabelAction);
-
-                // return all available actions
-                return result;
-            }
-        });
+        provider
+    );
 
     return labelContextMenuProvider;
 }
